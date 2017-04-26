635ceb45698c92b2f8a3d199910627033d24d07d
Steven Dunn <Dunn.Steven1@gmail.com>
2017-04-26 11:36:47 -0700

    Products can now be added and deleted from the database.

--- ProductApi/Controllers/ProductsController.cs
+++ ProductApi/Controllers/ProductsController.cs
@@ -32,7 +32,7 @@ namespace ProductApi.Controllers
             return Ok(allProducts);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetProductByID")]
         //id is automatically set with the id parameters coming from the request URL
         public IActionResult GetProduct(int id)
         {
@@ -43,7 +43,7 @@ namespace ProductApi.Controllers
                 return NotFound();
             }
 
-            var productResult = AutoMapper.Mapper.Map<CityWithoutPointsOfInterestDto>(productToReturn);
+            var productResult = AutoMapper.Mapper.Map<Product>(productToReturn);
             return Ok(productResult);
         }
 
@@ -64,57 +64,53 @@ namespace ProductApi.Controllers
 
         //USE POST TO CREATE. we use the same url route, but just make it a post. Also, we use ProductForCreated (so we can use validation attributes)
         //[FromBody] says to get the Product from the post body and try to de-serialize it to a ProductntsOfInterestForCreation object
-        //[HttpPost("{productId}/product")]
-        //public IActionResult CreateProduct(int productId, [FromBody] ProductForCreation product)
-        //{
-        //    //If the data sent cannot be de-serialized into a PointsOfInterestForCreation, the POI will be null
-        //    if (product == null)
-        //    {
-        //        return BadRequest(); //The consuming app messed up when sending data, so let them know it was a bad request.
-        //    }
+        [HttpPost("")]
+        public IActionResult CreateProduct([FromBody] ProductForCreation product)
+        {
+            //If the data sent cannot be de-serialized into a PointsOfInterestForCreation, the POI will be null
+            if (product == null)
+            {
+                return BadRequest(); //The consuming app messed up when sending data, so let them know it was a bad request.
+            }
 
-        //    if (product.Description == product.Name)
-        //    {
-        //        ModelState.AddModelError("Description", "Description and Name can not be the same");
-        //    }
+            //Check for things like the Required or MaxLength Attributes, which can make the model state invalid
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-        //    //Check for things like the Required or MaxLength Attributes, which can make the model state invalid
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
+            //The product comes from the post body and is of type productForCreation.
+            //Since we created a mapping for this in the mapper (see startup.cs), we can create a Product entity by mapping it from the ProductForCreation.
+            var finalproduct = AutoMapper.Mapper.Map<Entities.ProductEntity>(product);
 
-        //    // See if the city that was passed in exists in the database
-        //    var theProductExists = _productRepo.ProductExists(productId);
 
-        //    if (!theProductExists)
-        //    {
-        //        return NotFound();
-        //    }
+            // See if the product that was passed already exists in the database
+            var theProductExists = _productRepo.ProductExists(finalproduct.Id);
 
-        //    //The product comes from the post body and is of type productForCreation.
-        //    //Since we created a mapping for this in the mapper (see startup.cs), we can create a Product entity by mapping it from the ProductForCreation.
-        //    var finalproduct = AutoMapper.Mapper.Map<Entities.ProductEntity>(product);
+            if (theProductExists)
+            {
+                ModelState.AddModelError("Duplicate ID", "A Product with this ID already exists in the database");
+                return BadRequest(ModelState);
+            }
 
-        //    _productRepo.AddproductForCity(productId,finalproduct);
+            _productRepo.AddProduct(finalproduct);
 
-        //    //Attempt to save the new Product to the repo. If it doesn't return an object, then it fails
-        //    if (!_productRepo.Save())
-        //    {
-        //        _logger.LogInformation("There was a problem when trying to save the new Product to the database.");
-        //        return StatusCode(500, "A problem occured while handling your request");
-        //    }
+            //Attempt to save the new Product to the repo. If it doesn't return an object, then it fails
+            if (!_productRepo.Save())
+            {
+                _logger.LogInformation("There was a problem when trying to save the new Product to the database.");
+                return StatusCode(500, "A problem occured while handling your request");
+            }
 
-        //    var newlyCreatedProduct = AutoMapper.Mapper.Map<Models.productDto>(finalproduct);
+            var newlyCreatedProduct = AutoMapper.Mapper.Map<Models.Product>(finalproduct);
 
-        //    //This returns a 201 (created) response with the URL where the newly created resource can be found.
-        //    //Since we use the Get request to get a Product, we're saying that in order to get to this resource, they'll
-        //    //need to use the Getproduct Route (passing in a productId and Product Id that the Get Route needs).
-        //    //IMPORTANT: The GET route MUST have a name that matches the name in CreatedAtRoute here (e.g. "Getproduct")
-        //    return CreatedAtRoute("Getproduct", 
-        //                           new { productId = productId, poiId = newlyCreatedPoi.Id }, 
-        //                           newlyCreatedPoi);
-        //}
+            //This returns a 201 (created) response with the URL where the newly created resource can be found.
+            //Since we use the Get request to get a Product, we're saying that in order to get to this resource, they'll
+            //need to use the Getproduct Route (passing in a productId and Product Id that the Get Route needs).
+            //IMPORTANT: The GET route MUST have a name that matches the name in CreatedAtRoute here (e.g. "Getproduct")
+            return CreatedAtRoute("GetProductByID",
+                                   new { id = newlyCreatedProduct.Id},newlyCreatedProduct);
+        }
 
         //USE PUT TO UPDATE
         [HttpPut("{productId}/pointsOfInterest/{poiId}", Name = "Updateproduct")]
---
+++ ProductApi/Models/ProductForCreation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Models
+{
+    public class ProductForCreation
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You must provide a name value")]
+        [MaxLength(40, ErrorMessage = "Name can not be longer than 40 charactors")]
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
